24b4a80a9c7dfa59a869be52de826de7796071ba
Melvin Jones Repol <mrepol742@gmail.com>
2026-08-31 02:55:56 +0800

    feat: added trust proxies for cloudflare and simplify laravel exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,8 +16,31 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
+
+        // Trust proxies to handle X-Forwarded-For headers from load balancers
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO,
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // This simplify laravel exception logging by only logging relevant frames
+        $exceptions->report(function (Throwable $e) {
+            $trace = collect($e->getTrace())
+                ->filter(
+                    fn($frame) => isset($frame['file']) &&
+                        str_contains($frame['file'], base_path('app')),
+                )
+                ->take(5)
+                ->values()
+                ->all();
+
+            logger()->error($e->getMessage(), ['trace' => $trace]);
+
+            return false;
+        });
     })
     ->create();
